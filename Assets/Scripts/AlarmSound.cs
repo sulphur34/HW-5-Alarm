ff5d71dbf591347af669a2af093f56f927e79e46
sulphur34 <toku_gawa@mail.ru>
2023-07-07 17:16:57 +0500

    Comments correction 3
    SRP correction

--- Assets/Scripts/AlarmSound.cs
+++ Assets/Scripts/AlarmSound.cs
@@ -1,27 +1,48 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 
 public class AlarmSound : MonoBehaviour
 {
-    [SerializeField] private float _startVolume;
-    [SerializeField] private float _endVolume;
-
     private AudioSource _audioSource;
     private float _duration = 2f;
     private float _currentTime = 0f;
-    
+    private Predicate<float> _isMoreThanZero = (float volumeValue) => volumeValue > 0;
+    private Predicate<float> _isLessThanOne = (float volumeValue) => volumeValue < 1;
+    private float _startVolume;
+    private float _endVolume;
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.volume = 0f;
         _audioSource.Play();
+        _startVolume = 0;
+        _endVolume = 1;
     }
-   
-    public void SetVolume(int directionValue)
+
+    public void SwichAlarm(int directionValue)
     {
-        _currentTime += Time.deltaTime / _duration * directionValue;        
-        _audioSource.volume = Mathf.Lerp(_startVolume, _endVolume, _currentTime);
-    }    
+        StartCoroutine(SetVolume(directionValue));
+    }
+
+    private IEnumerator SetVolume(int directionValue)
+    {
+        Predicate<float> predicate;
+
+        if (directionValue == 1)
+            predicate = _isLessThanOne;
+        else
+            predicate = _isMoreThanZero;
+
+        while (predicate(_audioSource.volume))
+        {
+            _currentTime += Time.deltaTime / _duration * directionValue;
+            _audioSource.volume = Mathf.Lerp(_startVolume, _endVolume, _currentTime);
+            Debug.Log(_audioSource.volume);
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
--- Assets/Scripts/EnemyDetector.cs
+++ Assets/Scripts/EnemyDetector.cs
@@ -1,50 +1,26 @@
-using System;
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
-
-[RequireComponent(typeof(AudioSource))]
 
 public class EnemyDetector : MonoBehaviour
 {
     [SerializeField] private UnityEvent _trespassed;
-    [SerializeField] private UnityEvent _escaped;
-        
-    private AudioSource _audioSource;
-    private Predicate<float> _isMoreThanZero = (float volumeValue ) => volumeValue > 0;
-    private Predicate<float> _isLessThanOne = (float volumeValue) => volumeValue < 1;
-    private void Awake()
-    {
-        _audioSource = GetComponent<AudioSource>();
-        
-        
-        ;
-    }
-
+    [SerializeField] private UnityEvent _escaped;       
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent(out GragOhr gragOhr))
         {
-            StartCoroutine(SwichAlarm(_trespassed, _isLessThanOne));           
+           _trespassed.Invoke();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out GragOhr gragOhr))
-        {            
-            StartCoroutine(SwichAlarm(_escaped, _isMoreThanZero));
+        {
+            _escaped.Invoke();
         }
     }
 
-    private IEnumerator SwichAlarm(UnityEvent swiched, Predicate<float> predicate)
-    {
-        while(predicate(_audioSource.volume))
-        {
-            swiched.Invoke();
-            Debug.Log(_audioSource.volume);
-            yield return new WaitForEndOfFrame();
-        }
-    }
+    
 }
